a111eb81281f37905d1b3f8f811f5eb1d8237964
stefan666 <fanqingbiao1@h-partners.com>
2024-03-20 14:38:01 +0800

    修改

--- harmony/sound/src/main/ets/SoundTurboModule.ts
+++ harmony/sound/src/main/ets/SoundTurboModule.ts
@@ -24,14 +24,16 @@
 
 import { TurboModule, TurboModuleContext } from 'rnoh/ts';
 import { AVPlayerController } from './ts'
+import Logger from './Logger';
+
+const TAG: string = "[RNOH] Sound"
 
 export class SoundTurboModule extends TurboModule {
   controller: AVPlayerController = new AVPlayerController()
 
   constructor(protected ctx: TurboModuleContext) {
     super(ctx);
-    console.log('[RNOH]:SoundTurboModule constructor');
-
+    Logger.info(TAG, '[RNOH]:SoundTurboModule constructor');
   }
 
   setActive(active: boolean): void {
@@ -39,85 +41,85 @@ export class SoundTurboModule extends TurboModule {
   }
 
   isLoaded(): boolean {
-    console.log('sound isLoaded : ', this.controller.getIsLoaded())
+    Logger.info(TAG, 'sound isLoaded : '+ this.controller.getIsLoaded());
     return this.controller.getIsLoaded()
   }
 
   getDuration(): number {
-    console.log('sound getDuration: ',this.controller.getDuration())
+    Logger.info(TAG, 'sound getDuration : '+ this.controller.getDuration());
     return this.controller.getDuration();
   }
 
 
   play(onEnd?: (success: boolean) => void): void {
-    console.log('sound: ', 'play start')
+    Logger.info(TAG, 'sound  : '+ 'play start');
     this.controller.play();
   }
 
   pause(cb?: () => void): void {
-    console.log('sound: ', 'pause success')
+    Logger.info(TAG, 'sound  : '+ 'pause success');
     this.controller.pause();
   }
 
   stop(cb?: () => void): void {
-    console.log('sound: ', 'stop success')
+    Logger.info(TAG, 'sound  : '+ 'stop success');
     this.controller.stop();
   }
 
   reset(): void {
-    console.log('sound: ', 'reset success')
+    Logger.info(TAG, 'sound  : '+ 'reset success');
     this.controller.reset();
   }
 
   release(): void {
-    console.log('sound: ', 'release success')
+    Logger.info(TAG, 'sound  : '+ 'release success');
     this.controller.release();
   }
 
   getVolume(): number {
-    console.log('sound getVolume: ', this.controller.getVolume())
+    Logger.info(TAG, 'sound getVolume : '+ this.controller.getVolume());
     return this.controller.getVolume();
   }
 
   setVolume(value: number): void {
-    console.log('sound setVolume: ', value)
+    Logger.info(TAG, 'sound setVolume : '+ value);
     this.controller.setVolume(value)
   }
 
   getCurrentTime(): number {
-    console.log('sound getCurrentTime: ', this.controller.getCurrentTime())
+    Logger.info(TAG, 'sound getCurrentTime : '+ this.controller.getCurrentTime());
     return this.controller.getCurrentTime();
   }
 
   setCurrentTime(value: number): void {
     if (value < 0) value = 0;
     else if (value > this.controller.getDuration()) value = this.controller.getDuration();
-    console.log('sound setCurrentTime: ', value)
+    Logger.info(TAG, 'sound setCurrentTime : '+ value);
     this.controller.setCurrentTime(value);
   }
 
   getSpeed(): number {
-    console.log('sound getSpeed: ', this.controller.getSpeed())
+    Logger.info(TAG, 'sound getSpeed : '+ this.controller.getSpeed());
     return this.controller.getSpeed();
   }
 
   setSpeed(value: number): void {
-    console.log('sound setSpeed: ', value)
+    Logger.info(TAG, 'sound setSpeed : '+ value);
     this.controller.setSpeed(value);
   }
 
   isPlaying(): boolean {
-    console.log('sound isPlaying: ', this.controller.isPlaying)
+    Logger.info(TAG, 'sound isPlaying : '+ this.controller.isPlaying);
     return this.controller.isPlaying;
   }
 
   getNumberOfLoops() {
-    console.log('sound getNumberOfLoops: ',this.controller.getNumberOfLoops())
+    Logger.info(TAG, 'sound getNumberOfLoops : '+ this.controller.getNumberOfLoops());
     return this.controller.getNumberOfLoops();
   }
 
   setNumberOfLoops(value: boolean): void {
-    console.log('sound setNumberOfLoops: ',value)
+    Logger.info(TAG, 'sound setNumberOfLoops : '+ value);
     this.controller.setNumberOfLoops(value)
   }
 }
